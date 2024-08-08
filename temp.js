3736c7bba00208fee8dcc729809fb548163cae17
Rohan4404 <rohansharma99anc@gmail.com>
2024-08-08 16:54:57 +0530

    fifthcommit

--- temp.js
+++ temp.js
@@ -4,12 +4,15 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = 3000;
-
 app.use(cors());
-app.use(express.json());
-app.use(express.static('public')); // Serve static files from the "public" directory
 
+// Correct MongoDB connection string
+// const mongoUri = 'mongodb+srv://rohan4404:Hapur@123@cluster0.cnvpvij.mongodb.net/Cluster0?retryWrites=true&w=majority';
+
+// const mongoUri = "mongodb+srv://rohan4404:Hapur@123@cluster0.cnvpvij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const mongoUri = "mongodb+srv://rohansharma99anc:FCkDAmWBuW3bEwRT@cluster0.o594z.mongodb.net/dataset?retryWrites=true&w=majority&appName=Cluster0"
+
+// FCkDAmWBuW3bEwRT
 
 mongoose.connect(mongoUri)
   .then(() => {
@@ -23,6 +26,7 @@ mongoose.connect(mongoUri)
 const weatherSchema = new mongoose.Schema({
   Humidity: Number,
   temperature: Number,
+  airFlow: Number,
   timestamp: { type: Date, default: Date.now }
 });
 
@@ -30,36 +34,34 @@ const Weather = mongoose.model('Weather', weatherSchema);
 
 let Humidity = Math.random() * 30 + 50;
 let temperature = Math.random() * 25 + 10;
+let airFlow = Math.random() * 200 + 200;
 
-// Function to save weather data to MongoDB
-function saveWeatherData(temperature) {
+function updateWeatherData() {
   Humidity = Math.random() * 30 + 50;
+  temperature = Math.random() * 25 + 10;
+  airFlow = Math.random() * 200 + 200;
 
+  // Create a new weather data entry
   const newWeatherData = new Weather({
     Humidity: Humidity.toFixed(2),
     temperature: temperature.toFixed(2),
+    airFlow: airFlow.toFixed(2)
   });
 
+  // Save the weather data to MongoDB
   newWeatherData.save()
     .then(() => console.log('Weather data saved to MongoDB'))
     .catch((error) => console.error('Error saving weather data:', error));
 }
 
-// Route to handle form submission
-app.post('/update-weather', (req, res) => {
-  const { temperature } = req.body;
-  if (!temperature) {
-    return res.status(400).json({ error: 'Temperature is required' });
-  }
-
-  saveWeatherData(temperature);
-  res.status(200).json({ message: 'Temperature updated successfully' });
-});
+// Update weather data and store it in MongoDB every 2 seconds
+setInterval(updateWeatherData, 10000);
 
 app.get('/weather', (req, res) => {
   res.json({
     Humidity: Humidity.toFixed(2),
     temperature: temperature.toFixed(2),
+    airFlow: airFlow.toFixed(2)
   });
 });
 
